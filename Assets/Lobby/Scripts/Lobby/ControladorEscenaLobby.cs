63f5700de99fe32719e459799458556ca2bdf05d
Gustavo Bañuelos Ochoa <banuelos.201021581@gmail.com>
2019-11-14 01:48:47 -0600

    Se inicio el proyecto

---
+++ Assets/Lobby/Scripts/Lobby/ControladorEscenaLobby.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+namespace Prototype.NetworkLobby
+{
+    public class ControladorEscenaLobby : MonoBehaviour
+    {
+        //Scene scene = SceneManager.GetActiveScene();
+        private GameObject LobbyManager;
+        private GameObject mainPanel;
+        
+        //menu2
+        public LobbyManager lobbyManager;
+
+        // Start is called before the first frame update
+        public void Awake()
+        {
+            LobbyManager = GameObject.Find("LobbyManager");
+            mainPanel = LobbyManager.transform.Find("MainPanel").gameObject;
+            //lobbyPanel = LobbyManager.transform.Find("LobbyPanel").gameObject;
+
+            lobbyManager = LobbyManager.GetComponent<LobbyManager>();
+            
+            if (LobbyManager != null && mainPanel != null)
+            {
+                //Debug.Log("no es null");
+               // StartCoroutine(VerPanelMenu());
+                //lobbyManager.ChangeTo(panelLobby);
+            }
+            else
+            {
+                Debug.Log("null");
+            }
+
+        }
+
+        IEnumerator VerPanelMenu()
+        {
+            yield return new WaitForSeconds(0.2f);
+            //Debug.Log("Se inicio la escena lobby");
+            if (mainPanel != null)
+            {
+                //Debug.Log("El panel Menu NO es null");
+                if (!mainPanel.gameObject.active)
+                {
+                    Debug.Log("El panel estaba desactivado y se ha activado!");
+                    mainPanel.SetActive(true);
+                }
+            }
+            else
+            {
+                Debug.Log("El panel Menu es null");
+            }
+        }
+
+    }
+}
---
+++ Assets/Lobby/Scripts/Lobby/SceneController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+
+namespace Prototype.NetworkLobby
+{
+    public class SceneController : MonoBehaviour
+    {
+        public UnityEngine.Video.VideoClip videoClip;
+        public RenderTexture TexturaVideo;
+
+        public double time;
+        public double currentTime;
+
+        private bool flag = false;
+
+        private GameObject LobbyMan;
+        public LobbyManager lobbyManager;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //
+
+            LobbyMan = GameObject.Find("LobbyManager");
+            lobbyManager = LobbyMan.GetComponent<LobbyManager>();
+            //11
+            StartCoroutine(LoadDevice("cardboard", true));
+
+            //obtener componente video player para saber cuando esta activado
+            var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+            videoPlayer.playOnAwake = false;
+            videoPlayer.clip = videoClip;
+            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
+            videoPlayer.targetTexture = TexturaVideo;
+
+            time = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().clip.length;
+
+            StartCoroutine(IniciarVideo(videoPlayer));
+        }
+
+        IEnumerator IniciarVideo(UnityEngine.Video.VideoPlayer video)
+        {
+            yield return new WaitForSeconds(5);
+            flag = true;
+            video.Play();
+        }
+
+        IEnumerator BackToHost()
+        {
+            Debug.Log("Volviendo a client");
+            yield return new WaitForEndOfFrame();
+            flag = false;
+
+            StartCoroutine(LoadDevice("None", true));
+            yield return new WaitForSeconds(3);
+            //regresar al lobby
+            LobbyManager.s_Singleton.ServerReturnToLobby();
+            //LobbyManager.s_Singleton.OnLobbyClientEnter();
+
+      
+
+
+        
+
+       
+            //OnClientReady(false);
+            //SceneManager.LoadScene("client", LoadSceneMode.Single);
+
+        }
+
+        public void ToggleVR()
+        {
+
+            if (XRSettings.loadedDeviceName == "cardboard")
+            {
+                //StartCoroutine(LoadDevice("None"));
+                Debug.Log("None");
+            }
+            else
+            {
+                //StartCoroutine(LoadDevice("cardboard"));
+                Debug.Log("cardboard");
+            }
+        }
+
+
+
+        IEnumerator LoadDevice(string newDevice, bool enable)
+        {
+            XRSettings.LoadDeviceByName(newDevice);
+            yield return null;
+            XRSettings.enabled = enable;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            currentTime = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().time;
+            if (currentTime >= time && flag.Equals(true))
+            {
+                StartCoroutine(BackToHost());
+            }
+
+        }
+    }
+}
